4f6f360a8630a010d31ed80448a9d06232c4c3f7
abdul-09 <abdul.9.hussein@gmail.com>
2023-07-25 12:46:40 -0700

    bubble-sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,44 +1,36 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order
- * @array: the array to be sorted
- * @size: size of the array
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: void
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
+	size_t i, n, new_n;
+	int swap;
 
 	if (array == NULL || size < 2)
 		return;
 
-	/* no of passes required to sort the array*/
-	for (i = 0; i < size - 1; i++)
+	n = size;
+	while (n > 0)
 	{
-		swapped = 0;
-		/* check if any swaps occurred in this pass*/
-
-		/*Compare and swap adjacent elements*/
-		for (j = 0; j < size - i - 1; j++)
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				/*Swap the elements*/
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped = 1; /*Set swapped flag*/
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
 				print_array(array, size);
 			}
 		}
-
-		/*If no swaps occurred, the array is already sorted*/
-		if (swapped == 0)
-		{
-			return;
-		}
+		n = new_n;
 	}
 }
